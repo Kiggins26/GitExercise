d742a86a440af5830569f1edc101f83dd92e9260
Andrew Kiggins <andrewkiggins2000@gmail.com>
2021-09-16 23:27:47 -0700

    added logic

--- pickElement.go
+++ pickElement.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func getSmallest(input []int) int {
- 
+    
+    smallest := 9999999999999;
+
+    for i := 0; i < len(input); i++ {
+        if input[i] <= smallest{
+           
+            smallest = input[i]
+        }
+
+    }
+
+    return smallest;
 }
 
 
