aa7ccf8d7ac7a147111cff331966a7ea4b59ca93
Andrew Kiggins <andrewkiggins2000@gmail.com>
2021-09-16 23:19:44 -0700

    added logic

--- pickElement.go
+++ pickElement.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
-func getSmallest(){
+func getSmallest() int {
 
+    return -1;
 }
 
+func getLargest(input []int) int {
+    largest := -1;
+    
+    for i := 0; i < len(input); i++ {
+        if input[i] >= largest{
+            largest = input[i];
+        }
+    }
+    
+    return largest;
+}
 
 
 func main(){
